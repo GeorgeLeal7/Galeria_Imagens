021ca5fb215dee9914d314076b6a4a3b8f891887
GEORGE CORREIA LEAL NETO <21193368@CFP127ALUNO.LOCAL>
2021-10-05 16:15:05 -0300

    Galeria com Javascriptgit add .

--- galeria.js
+++ galeria.js
@@ -1,6 +1,7 @@
 "use strict"
 
 const imagens = [
+    "https://disneyplusbrasil.com.br/wp-content/uploads/2021/08/Miles-Morales.jpg",
     "./img/miles-capuz-1.jpg",
     "./img/miles-capuz-2.jpg",
     "./img/miles-de-costas.jpg",
@@ -24,14 +25,45 @@ const criarItem = (urlImagem) => {
     novoLink.href = `#${filtrarId(urlImagem)}`
     novoLink.classList.add("galeria-itens")
     novoLink.innerHTML = `<img src="${urlImagem}" alt="">`
-    container.append(novoLink)
+    container.appendChild(novoLink)
+}
+
+const criarSlide = (urlImagem, indice, array) => {
+    const container = document.querySelector(".slide-container")
+    const novaDiv = document.createElement("div")
+    novaDiv.classList.add("slide")
+    novaDiv.id = filtrarId(urlImagem)
+
+    const indiceAnterior = indice == 0 ? array.length - 1 : indice - 1
+    const slideAnterior = filtrarId(array[indiceAnterior])
+    
+    const indiceSeguinte = indice == array.length -1 ? 0 : indice + 1
+    const slideSeguinte = filtrarId(array[indiceSeguinte])
+
+    novaDiv.innerHTML = `
+    <div class="imagem-container">
+        <a href="" class="fechar">&#10006;</a>
+        <a href="#${slideAnterior}" class="navegacao anterior">&#171;</a>
+        <img src="${urlImagem}" alt="">
+        <a href="#${slideSeguinte}" class="navegacao proximo">&#187;</a>
+    </div>`
+
+container.appendChild(novaDiv)
+
 }
 
 //ForEach não retorna um array novo
 const carregarGaleria = (imgs) => imgs.forEach(criarItem)
+const carregarSlide = (imgs) => imgs.forEach(criarSlide)
 
 
 carregarGaleria(imagens)
+carregarSlide(imagens)
+
+
+
+
+
 
 
 
